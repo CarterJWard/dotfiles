db215c3e518e06434227e596d35ea595a3741cd1
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2023-11-05 15:07:22 +1100

    Add LSP Zero

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -13,4 +13,19 @@ return require('packer').startup(function(use)
     use { 'nvim-telescope/telescope.nvim', tag = '0.1.4' }
     use 'mbbill/undotree'
     use("nvim-treesitter/nvim-treesitter-context")
+    use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  requires = {
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'L3MON4D3/LuaSnip'},
+  }
+}
   end)
