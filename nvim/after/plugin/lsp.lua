73c4b23986fdfd542f2e9084ee39bc7ea54291c5
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2023-11-05 15:52:36 +1100

    Better LSP Config Added default lsp zero keybindings and added a key for formatting

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,8 +1,16 @@
-require("mason").setup()
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    lsp_zero.default_keymaps({ buffer = bufnr })
 end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    handlers = {
+        lsp_zero.default_setup,
+    }
+})
+vim.keymap.set('n', 'F', function()
+    vim.lsp.buf.format({ async = true })
+end, { desc = 'Format Document' })
