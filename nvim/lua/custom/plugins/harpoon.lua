b502cfb7d574292e9a02ae2f819842a4d6a02c6c
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2023-11-12 18:55:31 +1100

    added Autopairs and harpoon keybind descriptions

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -4,14 +4,14 @@ return {
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
-		vim.keymap.set("n", "<leader>ha", mark.add_file)
-		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+		vim.keymap.set("n", "<leader>ha", mark.add_file, {desc = "Add to harpoon"})
+		vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, {desc ="Open Harpoon menu"})
 
 		vim.keymap.set("n", "<leader>h1", function() ui.nav_file(1) end)
 		vim.keymap.set("n", "<leader>h2", function() ui.nav_file(2) end)
 		vim.keymap.set("n", "<leader>h3", function() ui.nav_file(3) end)
 		vim.keymap.set("n", "<leader>h4", function() ui.nav_file(4) end)
-		vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end)
-		vim.keymap.set("n", "<leader>hl", function () ui.nav_prev() end)
+		vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end, {desc = "Next Harpoon Item"})
+		vim.keymap.set("n", "<leader>hl", function () ui.nav_prev() end, {desc = "Prev Harpoon Item"})
 	end
 }
