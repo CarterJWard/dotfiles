f486d828fa0029deda95555c9ea18cd3ad4d5329
Carter Ward <20886719+CarterJWard@users.noreply.github.com>
2023-12-02 10:41:01 +1100

    add context and small config changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -129,6 +129,11 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = { 'branch', 'diff', 'diagnostics'},
+        lualine_c = {'filename'}
+      }
     },
   },
 
@@ -425,7 +430,7 @@ local on_attach = function(_, bufnr)
 
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  nmap('gI', require('telescope.builtin').lsp_iplementations, '[G]oto [I]mplementation')
   nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
@@ -477,7 +482,9 @@ local servers = {
   -- gopls = {},
   -- pyright = {},
   rust_analyzer = {},
-  tsserver = {},
+  tsserver = {
+    root_dir = vim.fn.getcwd(),
+  },
   eslint = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
