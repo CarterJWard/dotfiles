22f44bb05d5f54e2ed33605310da31705051e8c1
Carter Ward <carter@flkitover.com>
2023-12-13 17:52:33 +1100

    add hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,22 @@
+function toggleApp(appName)
+    return function()
+        local appObj = hs.application.get(appName)
+        if appObj then
+            if appObj:isFrontmost() then
+                appObj:hide()
+            else
+                appObj:activate()
+            end
+        else
+            hs.application.launchOrFocus(appName)
+        end
+    end
+end
+
+local modifier = {"ctrl", "alt", "cmd"}
+
+hs.hotkey.bind(modifier, "A", toggleApp("Arc"))
+hs.hotkey.bind(modifier, "T", toggleApp("Trello"))
+hs.hotkey.bind(modifier, "K", toggleApp("kitty"))
+hs.hotkey.bind(modifier, "S", toggleApp("slack"))
+hs.hotkey.bind(modifier, "O", toggleApp("obsidian"))
