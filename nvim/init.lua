599de1217229519c1d6729b933f74649fda19be5
Carter Ward <cjayward@proton.me>
2023-12-15 08:22:41 +1100

    toggle line blame and ignore karabiner backups

--- nvim/init.lua
+++ nvim/init.lua
@@ -83,6 +83,7 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      current_line_blame = true,
       on_attach = function(bufnr)
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
         -- don't override the built-in and fugitive keymaps
