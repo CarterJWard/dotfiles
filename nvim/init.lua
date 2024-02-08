ab3e1b24f197fd95390f015629f112fc4f87c0d8
Carter Ward <cjayward@proton.me>
2024-02-08 17:12:50 +1100

    add file path to lua line

--- nvim/init.lua
+++ nvim/init.lua
@@ -133,7 +133,7 @@ require('lazy').setup({
       sections = {
         lualine_a = {'mode'},
         lualine_b = { 'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'}
+        lualine_c = {{'filename', path = 1}}
       }
     },
   },
