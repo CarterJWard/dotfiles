112b41ab6b5e6e41245cd2d4cfd6827fb0391f5a
Carter Ward <cjayward@proton.me>
2024-07-09 20:16:07 +1000

    re add trello hotkey

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -20,6 +20,7 @@ local profiles = {}
 profiles["Work"] = {
     { key = "E", app = "Mail" },
     { key = "T", app = "Jira" },
+    { key = "W", app = "Trello" },
     { key = "S", app = "slack" },
     { key = "M", app = "Studio 3T" },
     { key = "F", app = "Firefox Developer Edition" },
